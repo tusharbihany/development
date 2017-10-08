e237f65767d1942fcda9ff0bbfae4ff0fad8ded9
Bihany, Tushar <Tushar_Bihany@intuit.com>
2017-10-09 01:10:57 +0530

    Calculator Obj-C

---
+++ Calculator Objective-C/Calculator/build/Calculator.build/Debug/Calculator.build/Objects-normal/x86_64/AppDelegate.d
@@ -0,0 +1,3 @@
+dependencies: /Users/tbihany/Desktop/Calculator/Calculator/AppDelegate.m \
+  /Users/tbihany/Desktop/Calculator/Calculator/AppDelegate.h \
+  /Users/tbihany/Desktop/Calculator/Calculator/ViewController.h
